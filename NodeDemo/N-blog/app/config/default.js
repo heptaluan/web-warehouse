07efe9119501e8bafa9a970a48998d732b4d1c4a
hanekaoru <vd2o@foxmail.com>
2016-12-06 18:16:05 +0800

    add

---
+++ NodeDemo/N-blog/app/config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 程序启动要监听的端口号
+    port: 3000,
+    // express-session 的配置信息
+    session: {
+        secret: 'myblog',
+        key: 'myblog',
+        maxAge: 2592000000
+    },
+    // mongodb 的地址
+    mongodb: 'mongodb://localhost:27017/myblog'
+};
---
+++ NodeDemo/N-blog/app/routes/index.js
@@ -0,0 +1,9 @@
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+};

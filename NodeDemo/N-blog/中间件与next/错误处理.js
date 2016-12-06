557fdc30014f8851521c5e2fc326d4ccd4b7ecf6
hanekaoru <vd2o@foxmail.com>
2016-12-06 11:34:39 +0800

    add

---
+++ NodeDemo/N-blog/中间件与next/错误处理.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.use(function (req, res, next) {
+    console.log(1)
+    next(new Error("abc"))
+})
+
+app.use(function (req, res, next) {
+    console.log(2)
+    res.status(200).end();
+})
+
+// 错误处理
+app.use(function (err, req, res, next) {
+    console.log(err.stack)
+    res.status(500).send("Something broke!")
+})
+
+app.listen(3000)

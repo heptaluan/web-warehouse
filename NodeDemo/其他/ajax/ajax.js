abccb0f393c82a966e39be88cbfe7c64822d4e5c
hanekaoru <vd2o@foxmail.com>
2016-12-08 17:46:36 +0800

    Update ajax.js

--- NodeDemo/其他/ajax/ajax.js
+++ NodeDemo/其他/ajax/ajax.js
@@ -1,15 +1,15 @@
-// 当请求根目录/时，返回这个 HTML 文件
-// 当请求/date时，返回当前时间
 var http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('./ajax.html', 'utf8');
 
 http.createServer(function (req, res) {
     switch (req.url) {
+        // 当请求根目录 / 时，返回这个 HTML 文件
         case '/':
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(html)
             break;
+        // 当请求 /date 时，返回当前时间
         case '/date':
             var date = new Date();
             res.writeHead(200, { 'Content-Type': 'text/plain' });

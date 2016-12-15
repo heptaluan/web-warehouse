466e071064065c012362c3e4d9dac1979dcd85a0
hanekaoru <vd2o@foxmail.com>
2016-12-15 17:00:07 +0800

    add

--- demos/js/ajax文章/ajax.js
+++ demos/js/ajax文章/ajax.js
@@ -6,23 +6,23 @@ function ajax(json) {
     var data = json.data || '';
     var oAjax;
     //创建ajax对象
-    if(window.XMLHttpRequest){
+    if (window.XMLHttpRequest) {
         oAjax = new XMLHttpRequest();
-    }else{
+    } else {
         oAjax = new ActiveXObject('Microsoft.XMLHTTP');
     }
     //连接服务器
-    oAjax.open(type,url,true);
+    oAjax.open(type, url, true);
     //发送请求
-    oAjax.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    oAjax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     oAjax.send(data);
-    oAjax.onreadystatechange = function(){
-        if(oAjax.readyState == 4){
-            if(oAjax.status == 200){
+    oAjax.onreadystatechange = function () {
+        if (oAjax.readyState == 4) {
+            if (oAjax.status == 200) {
                 var data = oAjax.responseText;
                 success(data);
-            }else{
-                if(error){
+            } else {
+                if (error) {
                     error();
                 }
             }

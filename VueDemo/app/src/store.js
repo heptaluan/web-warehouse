7f185ef65e52447d91d98b87b4ed66f9b3e9b0f1
hanekaoru <vd2o@foxmail.com>
2016-12-25 20:55:10 +0800

    Update store.js

--- VueDemo/app/src/store.js
+++ VueDemo/app/src/store.js
@@ -1,9 +1,12 @@
+// 保存函数
 const STORAGE_KEY = "todos-vuejs"
 
 export default {
+    // 读取 getItem
     fetch () {
         return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || "[]");
     },
+    // 保存 setItem
     save (items) {
         window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
     }

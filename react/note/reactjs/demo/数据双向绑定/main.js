5a54a614e1ed4ec9f235096d2a2456b4d10bbffc
hanekaoru <vd2o@foxmail.com>
2017-09-07 09:21:11 +0800

    add react demo

---
+++ react/note/reactjs/demo/数据双向绑定/main.js
@@ -0,0 +1,36 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+class Father extends React.Component {
+
+    constructor() {
+        super();
+        
+        this.change = (this.change).bind(this);
+
+        this.state = {
+            "txt": ""
+        }
+    }
+
+    change(event) {
+        this.setState({
+            "txt": event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" onInput={this.change} />
+                <h1>{ this.state.txt }</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Father />,
+    document.getElementById("box")
+)

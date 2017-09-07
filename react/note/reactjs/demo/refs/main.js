969b9608c1956e15253dd197832a510b901468f4
hanekaoru <vd2o@foxmail.com>
2017-09-07 09:49:42 +0800

    add react refs demo

---
+++ react/note/reactjs/demo/refs/main.js
@@ -0,0 +1,28 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+class Father extends React.Component {
+
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="button" value="点击切换颜色" onClick={ () => {
+                    this.refs.box.style.backgroundColor="blue"
+                }} />
+                <br />
+                <br />
+                <div ref="box" style={{"width": "200px", "height": "200px", "backgroundColor": "red"}}></div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Father />,
+    document.getElementById("box")
+)

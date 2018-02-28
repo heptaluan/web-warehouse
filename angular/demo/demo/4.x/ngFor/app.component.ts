179ebfe33e89457cfcc5e0f672599aaf0d0f1e17
hanekaoru <vd2o@foxmail.com>
2018-02-28 11:09:18 +0800

    bug fix and add demo

---
+++ angular/demo/demo/4.x/ngFor/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+// 定义组件的元信息
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+// 定义组件类
+export class AppComponent {
+  items = ITEMS
+  selectItem: Item
+
+  onSelect(item: Item): void {
+    this.selectItem = item
+  }
+}
+
+export class Item {
+  id: Number
+  name: String
+}
+
+const ITEMS: Item[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+]

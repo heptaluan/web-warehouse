4dd7b14b32d75971c987029c1720591ef0db9e35
hanekaoru <vd2o@foxmail.com>
2019-05-24 18:40:44 +0800

    update node

--- node/note/2019/index.js
+++ node/note/2019/index.js
@@ -1,3 +1,15 @@
 /// <reference path="./typings/index.d.ts" />
 
-console.log(process.argv)
+var util = require('util');
+
+function Person() {
+  this.name = 'zhangsan';
+  this.toString = function () {
+    return this.name;
+  };
+}
+
+var obj = new Person();
+
+console.log(util.inspect(obj));
+console.log(util.inspect(obj, true));

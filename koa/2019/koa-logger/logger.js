70f569f1ca99d86a1b51a3ca221419a74e78435c
heptaluan <vd2o@foxmail.com>
2019-07-17 23:19:56 +0800

    update koa

---
+++ koa/2019/koa-logger/logger.js
@@ -0,0 +1,25 @@
+// generator 版本
+// function log(ctx) {
+//   console.log(ctx)
+// }
+
+// module.exports = function() {
+//   return function* (next) {
+//     log(this)
+//     if (next) {
+//       yield next
+//     }
+//   }
+// }
+
+// async 版本
+function log(ctx) {
+  console.log(ctx)
+}
+
+module.exports = function() {
+  return async function(ctx, next) {
+    log(ctx)
+    await next()
+  }
+}
